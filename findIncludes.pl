eae7914d3fd955fa2e65dfbd1565f5ef7a5bf2f1
Matt Postiff <postiffm@umich.edu>
2020-09-30 22:13:03 -0400

    Primarily, add two scripts to cut out columns from files

--- findIncludes.pl
+++ findIncludes.pl
@@ -1,11 +1,13 @@
 #!/usr/bin/perl
 
 while ($ln = <>) {
-    $pcs = split('\s+', $ln);
+    #print $ln;
+    my @pcs = split(/\s+/, $ln);
     foreach $pc (@pcs) {
+	#print $pc;
 	if ($pc =~ /-I(.*)/) {
 	    # Print the include I just found
-	    print $pc;
+	    print $1, "\n";
 	}
     }
 }
